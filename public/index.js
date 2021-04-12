614253610908ad50681167fd88e4a63fe39dec6c
Mcvean Soans <mc.soans02@gmail.com>
2021-04-12 23:42:06 +0530

    add: additional UX

--- public/index.js
+++ public/index.js
@@ -41,14 +41,31 @@ modalBg.addEventListener('click', () => {
     modal.classList.remove('is-active');
 });
 
-cancelBtn.addEventListener('click', () => {
+cancelBtn.addEventListener('click', (e) => {
+    e.preventDefault();
     modal.classList.remove('is-active');
 });
 
-joinBtn.addEventListener('click', () => {
+joinBtn.addEventListener('click', (e) => {
+    e.preventDefault();
     joinBtn.classList.add('is-loading');
     setTimeout(() => {
         joinBtn.classList.remove('is-loading');
         modal.classList.remove('is-active');
-    }, 10000);
+    }, 5000);
+});
+
+// cart
+const addToCart = document.querySelector('#add-to-cart');
+let cartText = document.querySelector('#shopping-cart');
+let itemCount = parseInt(cartText.innerHTML.slice(-2, -1));
+
+addToCart.addEventListener('click', (e) => {
+    e.preventDefault();
+    itemCount += 1;
+    addToCart.classList.add('is-loading');
+    setTimeout(() => {
+        cartText.innerHTML = `Shopping Cart (${itemCount})`;
+        addToCart.classList.remove('is-loading');
+    }, 1000);
 });
